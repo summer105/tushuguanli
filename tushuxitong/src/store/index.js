d899d2167cbc265cff1a22671f07488dffbde4a1
summer105 <1272441534@qq.com>
2020-03-17 21:18:16 +0800

    '图书管理系统1'

--- tushuxitong/src/store/index.js
+++ tushuxitong/src/store/index.js
@@ -1,16 +1,19 @@
 import Vue from "vue"
 import  Vuex from "vuex"
+import chaxun from "@/store/chaxun"
+import jiedu from "@/store/jiedu"
 Vue.use(Vuex);
 
 //为什么用const 常量  因为store不可以被改变
 const store=new Vuex.Store({
   modules:{//引入子模块的  在核心模块中只放首页的状态逻辑
-
+    chaxun,
+    jiedu,
   },
 
   strict:true,//开启严格模式  唯一改变状态的方式是mutations  否则报错
   state:{//存状态  存的公共状态
-    // m:100
+
   },
   mutations:{//唯一改变状态的方式  不支持异步
 
---
+++ tushuxitong/src/store/jiedu.js
@@ -0,0 +1,20 @@
+import * as API from "../diaojiekou"; //*取所有的 json数据  as是给*取了一个别名 叫API
+
+const jiedu={
+  namespaced:true,//是否全局
+  state:{
+
+  },
+  mutations:{
+
+  },
+  getters:{
+
+  },
+  actions: { //异步的行为里面
+
+  },
+
+}
+
+export default jiedu
